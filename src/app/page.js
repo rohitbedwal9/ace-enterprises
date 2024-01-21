40f91c1b0796ec6f642c64bc5d698c32319d9b2a
Taranjeet Singh <taranjeets120@gmail.com>
2024-01-21 00:16:37 -0800

    recent project container done

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import { Navbar } from "@/components";
+import Projects from "@/components/projects";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex  flex-col items-center justify-between ">
       <Navbar/>
+      <Projects/>
     </main>
   );
 }
