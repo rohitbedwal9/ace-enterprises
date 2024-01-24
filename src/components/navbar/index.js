212ee3778cab11876187debbb4a86f63762df8d8
Rohit Singh Bedwal <rohitbedwal9@gmail.com>
2024-01-24 23:49:21 +0530

    added new nav

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -9,47 +9,54 @@ export const Navbar = () => {
     const toggle = () => {
         setShowMe(!showMe);
     }
+
+    const [isLogin, setIsLogin] = useState(true);
+    const toggleLogin = () => {
+        setIsLogin(!isLogin);
+    }
     return (
         <>
             <div style={{display:showMe?"block":"none",
-            }}className="h-screen w-full dark:bg-gray-900 bg-gray" >
-                <div className='flex flex-col h-max py-10 pl-[4rem] pr-20 pt-6 gap-6 text-lg font-semibold'>
+            }} className="h-full w-full fixed top-0  z-10  text-white bg-gradient-to-r from-[#2f8396] to-[#2f8396]" >
+                <div className='flex flex-col h-max pl-[2.5rem] pr-22 pt-6 gap-6 text-lg font-semibold'>
                     <div >
-                        <button className='sm:hidden' title='Toggle Menu' onClick={toggle}>
+                        <button className='md:hidden' title='Toggle Menu' onClick={toggle}>
                         <CgClose size={25}/>
                         </button>
                     </div>
-                    <div className='w-max pb-2 border-b-2  text-xl  p-2 rounded-lg  bg-white text-black'>
+                    <div className='w-max pb-2  text-xl  p-2  bg-transparent shadow-sm border-y-2  text-yellow-300'>
                         Ace-Enterprises
                     </div>
-                    <Link href='/' className=' border-b-2 border-black dark:border-white w-max dark:hover:text-gray-300 hover:text-gray-700'>Home</Link>
-                    <Link href='/products' className='border-b-2 border-black dark:border-white w-max dark:hover:text-gray-300 hover:text-gray-700'>Projects</Link>
-                    <Link href='/about' className='border-b-2 border-black dark:border-white w-max dark:hover:text-gray-300 hover:text-gray-700'>About</Link>
-                    <Link href='/contact' className='border-b-2 border-black dark:border-white w-max dark:hover:text-gray-300 hover:text-gray-700'>Contact</Link>
-                    <Link href='/faq' className='border-b-2 border-black dark:border-white w-max dark:hover:text-gray-300 hover:text-gray-700 '>FAQ</Link>
+                    <Link href='/' className=' border-b-2 border-white  w-max hover:text-gray-300'>Home</Link>
+                    <Link href='/products' className=' border-b-2 border-white  w-max hover:text-gray-300'>Projects</Link>
+                    <Link href='/about' className='border-b-2 border-white  w-max hover:text-gray-300'>About</Link>
+                    <Link href='/contact' className='border-b-2 border-white  w-max hover:text-gray-300'>Contact</Link>
+                    <Link href='/faq' className='border-b-2 border-white  w-max hover:text-gray-300'>FAQ</Link>
                 </div>
             </div>
             <div style={{
-                display: showMe ? "none" : "flex"}} className="navbar flex bg-transparent justify-around sm:justify-between items-center w-full pt-5 sm:px-10 sm:py-6  " >
-                <div className="sm:hidden">
+                display: showMe ? "none" : "flex"}} className="navbar flex bg-transparent justify-between items-center w-full pt-5 px-10 sm:py-6  " >
+                <div className="md:hidden">
                     <button title='Toggle Menu' onClick={toggle}>
-                        <GiHamburgerMenu size={25}/>
+                        <GiHamburgerMenu size={25} className="text-white"/>
                     </button>
                 </div>
-                <div className="logo hidden sm:block text-xl sm:text-2xl font-bold select-none ">
+                <div className="logo hidden md:block text-xl sm:text-3xl  font-bold select-none text-white ">
                     Ace-Enterprises
                 </div>
-                <div className="hidden sm:flex text-lg w-[30%] justify-between">
-                    <Link href="/" className="bg-transparent hover:bg-white hover:dark:bg-gray-600 px-2 rounded-md focus:opacity-70">Home</Link>
-                    <Link href="/products" className="bg-transparent hover:bg-white hover:dark:bg-gray-600 px-2 rounded-md focus:opacity-70">Projects</Link>
-                    <Link href="/about" className="bg-transparent hover:bg-white hover:dark:bg-gray-600 px-2 rounded-md focus:opacity-70">About</Link>
-                    <Link href="/contact" className="bg-transparent hover:bg-white hover:dark:bg-gray-600 px-2 rounded-md focus:opacity-70">Contact</Link>
-                    <Link href="/faq" className="bg-transparent hover:bg-white hover:dark:bg-gray-600 px-2 rounded-md focus:opacity-70">FAQ</Link>
+                <div className="hidden md:flex text-lg w-[30%] justify-between bg-black p-2 rounded-3xl text-white">
+                    <Link href="/" className="  hover:bg-gray-700 font-semibold px-2 rounded-3xl focus:opacity-70">Home</Link>
+                    <Link href="/products" className="  hover:bg-gray-700 font-semibold px-2 rounded-3xl focus:opacity-70">Projects</Link>
+                    <Link href="/about" className="  hover:bg-gray-700 px-2 font-semibold rounded-3xl focus:opacity-70">About</Link>
+                    <Link href="/contact" className="  hover:bg-gray-700 px-2 font-semibold rounded-3xl focus:opacity-70">Contact</Link>
+                    <Link href="/faq" className="  hover:bg-gray-700 px-2  font-semibold rounded-3xl focus:opacity-70">FAQ</Link>
                 </div>
                 <div>
-                    <Link href="/signup" className="flex gap-2 items-center bg-white dark:text-black hover:opacity-80 p-2 rounded-lg text-lg shadow-lg ">
-                        <span>Get Started</span>
-                    </Link>  
+                    <Link href="/signup" className="flex  items-center bg-yellow-300 hover:opacity-80 p-2 rounded-3xl sm:text-lg shadow-lg " onClick={toggleLogin}>
+                        <span style={{display:isLogin?"block":"none"}}>Login/Register</span>
+                        <span style={{display:isLogin?"none":"block"}}>SignOut</span>
+                    </Link>
+
                 </div>
             </div>
             
