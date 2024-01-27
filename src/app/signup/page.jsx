a94c9eb928200c7289e09b80469884f29ba62b38
Rohit Singh Bedwal <rohitbedwal9@gmail.com>
2024-01-28 01:20:30 +0530

    added login forget password pages

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -1,5 +1,5 @@
 "use client"
-
+import { FcGoogle } from 'react-icons/fc';
 import { EmailVerify, register, google } from '@/utils/firebaseMethods';
 import React, { useState } from 'react';
 
@@ -16,15 +16,16 @@ const SignUp = () => {
         await EmailVerify()
         const msg = await register(email, password, name, number)
         console.log(msg)
+
     }
 
     return (
         <>
-            <section className="bg-gray-50 dark:bg-gray-900">
-                <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-                    <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+            <section className="bg-white">
+                <div className="flex flex-col items-center justify-center px-2 py-4 mx-auto md:h-screen lg:py-0 w-fll">
+                    <div className="w-full  rounded-lg shadow bg-gray-900 md:mt-0 sm:max-w-md xl:p-0">
                         <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                            <h1 className="text-center text-2xl font-bold leading-tight tracking-tight text-gray-900 md:text-3xl dark:text-white">
+                            <h1 className="text-center text-2xl font-bold leading-tight tracking-tight text-yellow-300 md:text-3xl ">
                                 Create an account
                             </h1>
                             <form onSubmit={(e) => Register(e)} className="space-y-4 md:space-y-6" action="#">
@@ -64,13 +65,14 @@ const SignUp = () => {
                                         Phone Number
                                     </label>
                                     <input
-                                        type="number"
+                                        type="tel"
                                         name="number"
+                                        pattern="[0-9]{10}"
                                         id="number"
                                         value={number}
                                         onChange={(e) => setNumber(e.target.value)}
                                         className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                        placeholder="phone number"
+                                        placeholder="10 digit phone number"
                                         required
                                     />
                                 </div>
@@ -89,26 +91,14 @@ const SignUp = () => {
                                         required
                                     />
                                 </div>
-                                <div>
-                                    <label htmlFor="confirm-password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                                        Confirm password
-                                    </label>
-                                    <input
-                                        type="password"
-                                        name="confirm-password"
-                                        id="confirm-password"
-                                        placeholder="••••••••"
-                                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                        required
-                                    />
-                                </div>
+                                
                                 <div className="flex items-start">
                                     <div className="flex items-center h-5">
                                         <input
                                             id="terms"
                                             aria-describedby="terms"
                                             type="checkbox"
-                                            className=" accent-orange-500 w-4 h-4 border border-orange-400 rounded bg-orange-400 focus:ring-3 focus:ring-orange-300 dark:bg-orange-400 dark:border-orange-400 dark:focus:ring-orange-400 dark:ring-offset-orange-400"
+                                            className=" accent-yellow-300 w-4 h-4 border border-yellow-400 rounded bg-white"
                                             required
                                         />
                                     </div>
@@ -123,20 +113,20 @@ const SignUp = () => {
                                 </div>
                                 <button
                                     type="submit"
-                                    className="w-full text-white bg-orange-400 hover:bg-orange-500 focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-orange-400 dark:hover:bg-orange-500 dark:focus:ring-primary-800"
+                                    className="w-full text-black bg-yellow-300 hover:bg-yellow-400 focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center  dark:focus:ring-primary-800"
                                 >
                                     Create an account
                                 </button>
                                 <button
                                     onClick={() => google()}
-                                    className="w-full text-white bg-green-400 hover:bg-green-500 focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-orange-400 dark:hover:bg-orange-500 dark:focus:ring-primary-800"
+                                    className="w-full text-black  bg-white focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-sm py-2.5 text-center  flex gap-2 justify-center items-center"
                                 >
-                                    Google
+                                   SignUp With <FcGoogle size={20} />
                                 </button>
 
                                 <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                                     Already have an account?{' '}
-                                    <a href="/login" className="font-medium text-orange-400 hover:underline dark:text-orange-400">
+                                    <a href="/login" className="font-medium text-yellow-300 hover:underline ">
                                         Login
                                     </a>
 
