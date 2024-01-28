1e7d117c6e1fde2c2b88990563f907340f812ab8
Rohit Singh Bedwal <rohitbedwal9@gmail.com>
2024-01-28 20:13:43 +0530

    updated service component design

--- src/components/statics/index.js
+++ src/components/statics/index.js
@@ -1,6 +1,6 @@
 export const Statics = () => {
   return (
-    <div className="flex flex-col w-full gap-10 md:flex-row justify-around md:px-56 py-10 items-center h-full md:h-56 bg-white">
+    <div className="flex  shadow-xl flex-col w-full gap-10 md:flex-row justify-around md:px-56 py-10 items-center h-full md:h-56 bg-white">
       <div className="flex flex-col gap-2 justify-center  items-center">
         <span className="text-3xl sm:text-4xl md:text-5xl font-bold">350+</span>
         <span className="text-xl sm:text-2xl text-center ">Sucessfull Projects</span>
