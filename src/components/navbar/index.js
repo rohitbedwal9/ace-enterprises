1dc74cf003526f5675717ffeac52b4d5a910ccd9
Taranjeet Singh <taranjeets120@gmail.com>
2024-01-30 18:18:22 +0530

    some changes

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -33,7 +33,7 @@ export const Navbar = () => {
     console.log(data)
 
     return (
-        < div className="flex flex-col w-full">
+        <div className="flex flex-col w-full">
             <div className="navbar flex bg-transparent justify-between items-center w-full pt-5 px-10 sm:py-6  " >
                 <div className="md:hidden">
                     <button style={{ 'display': showMe ? 'none' : 'block' }} title='Toggle Menu' onClick={toggle}>
@@ -54,58 +54,42 @@ export const Navbar = () => {
                     <Link href="/faq" className="  hover:bg-gray-400 px-2  font-semibold rounded-3xl focus:opacity-70">FAQ</Link>
                 </div>
                 <div>
-
                     {isLogin ? (
-                        <div>
-                            {/* <button onClick={logout} className="flex  items-center border-2 border-black  hover:bg-gray-900 hover:text-white p-2 rounded-3xl sm:text-lg shadow-lg "   >SignOut</button> */}
-                            <div className="inline-flex  rounded-md">
+                        <div className="inline-flex  rounded-md">
 
-                                <div className="relative">
-                                    <img
-                                        onClick={() => setDropdownOn(!dropdownOn)}
-                                        type="button"
-                                        src={data.photoURL ? data.photoURL :'https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg'}
-                                        className="w-10 h-10 rounded-full"
-                                    />
+                            <div className="relative">
+                                <img
+                                    onClick={() => setDropdownOn(!dropdownOn)}
+                                    type="button"
+                                    src={data.photoURL ? data.photoURL : 'https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg'}
+                                    className="w-10 h-10 rounded-full"
+                                />
 
 
 
-                                    <div style={{ display: dropdownOn ? 'block' : 'none' }} className="absolute right-0 z-10 w-56 mt-4 origin-top-right bg-white border border-gray-100 rounded-md shadow-lg">
-                                        <div className="p-2">
-                                            <a
-                                                href="#"
-                                                className="block px-4 py-2 text-sm text-gray-500 rounded-lg hover:bg-gray-50 hover:text-gray-700"
-                                            >
-                                              {data.email}
-                                            </a>
+                                <div style={{ display: dropdownOn ? 'block' : 'none' }} className="absolute right-0 z-10 w-56 mt-4 origin-top-right bg-white border border-gray-100 rounded-md shadow-lg">
+                                    <div className="p-2">
+                                        <div className="block px-4 py-2 text-sm text-gray-500 rounded-lg hover:bg-gray-50 hover:text-gray-700">
+                                            {data.email}
+                                        </div>
 
-                                            <a
-                                                href="#"
-                                                className="block px-4 py-2 text-sm text-gray-500 rounded-lg hover:bg-gray-50 hover:text-gray-700"
-                                                onClick={logout}
-                                            >
-                                                Logout
-                                            </a>
+                                        <div onClick={logout} className="cursor-pointer block px-4 py-2 text-sm text-gray-500 rounded-lg hover:bg-gray-50 hover:text-gray-700">
+                                            Logout
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-
                     ) : (
                         <Link href="/signup">
-                            <button className="flex  items-center border-2 border-black  hover:bg-gray-900 hover:text-white p-2 rounded-3xl sm:text-lg shadow-lg " >Login / Register</button>
+                            <button className="flex   items-center border-2 border-black  hover:bg-gray-900 hover:text-white p-2 rounded-2xl sm:text-lg shadow-lg " >Login</button>
                         </Link>
                     )}
-
-
                 </div>
-
             </div>
-            <div style={{
-                display: showMe ? "block" : "none",
-            }} className="bg-gray-900 text-white w-max  rounded-xl p-10 z-10 fixed top-20 left-1" >
-                <div className='flex flex-col     gap-6 text-lg font-semibold'>
+
+            <div style={{display: showMe ? "block" : "none"}} className="bg-gray-900 text-white w-max  rounded-xl p-10 z-10 fixed top-20 left-1" >
+                <div className='flex flex-col gap-6 text-lg font-semibold'>
                     <div className='w-max  text-xl  text-yellow-300'>
                         Ace-Enterprises
                     </div>
