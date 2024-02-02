97609703b5b37d2ee83d6abb0cc116e566f6724d
Taranjeet Singh <taranjeets120@gmail.com>
2024-02-02 23:34:27 +0530

    some minors upgrades

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -6,6 +6,7 @@ import Router, { useRouter } from 'next/navigation';
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from '../../utils/firebase';
 import Link from 'next/link'
+import { Navbar } from '@/components';
 
 const SignUp = () => {
     const [email, setEmail] = useState('')
@@ -13,7 +14,6 @@ const SignUp = () => {
     const [name, setName] = useState('')
     const [number, setNumber] = useState('')
     const [error, setError] = useState('')
-    const [msg, setMsg] = useState('')
     const [show, setShow] = useState(true)
     const router = useRouter()
 
@@ -77,21 +77,22 @@ const SignUp = () => {
         <>
             {show ? "" : (
                 <div className="w-full">
-                    <div className="w-full h-screen md:h-screen bg-[url('/images/construction-background2.jpeg')] bg-no-repeat bg-cover">
+                    <div className="w-full h-full md:h-screen bg-[url('/images/construction-background2.jpeg')] bg-no-repeat bg-cover">
                         <div className="backdrop-blur-sm h-full">
-                            <div className="flex flex-col items-center justify-center px-2 py-4 mx-auto md:h-screen lg:py-0 w-fll">
+                            <Navbar />
+                            <div className="flex flex-col items-center justify-center px-2 py-4 mt-5  mx-auto lg:py-0 w-fll">
                                 <div className="w-full  rounded-lg shadow bg-gray-900 md:mt-0 sm:max-w-md xl:p-0">
-                                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                                    <div className="p-6  sm:px-8">
 
                                         <h1 className="text-center text-2xl font-bold leading-tight tracking-tight text-yellow-300 md:text-3xl ">
                                             Create an account
                                         </h1>
-                                        <p className='text-center text-red-400'>{error ? 'Error: ' + error : ''}</p>
-                                        <p className='text-center text-green-400'>{msg ? msg : ''}</p>
+                                        <p className='text-center text-red-400 '>{error ? 'Error: ' + error : ''}</p>
+                                        
 
-                                        <form onSubmit={(e) => Register(e)} className="space-y-4 md:space-y-6" action="#">
+                                        <form onSubmit={(e) => Register(e)} className="" action="#">
                                             <div>
-                                                <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                                                <label htmlFor="name" className="block mt-4 mb-2 text-white text-sm font-medium text-gray-900 dark:text-white">
                                                     Full Name
                                                 </label>
                                                 <input
@@ -107,7 +108,7 @@ const SignUp = () => {
                                             </div>
 
                                             <div>
-                                                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                                                <label htmlFor="email" className="block mt-4 mb-2 text-white text-sm font-medium text-gray-900 dark:text-white">
                                                     Your email
                                                 </label>
                                                 <input
@@ -122,7 +123,7 @@ const SignUp = () => {
                                                 />
                                             </div>
                                             <div>
-                                                <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                                                <label htmlFor="name" className="block mt-4 mb-2 text-white text-sm font-medium text-gray-900 dark:text-white">
                                                     Phone Number
                                                 </label>
                                                 <input
@@ -138,7 +139,7 @@ const SignUp = () => {
                                                 />
                                             </div>
                                             <div>
-                                                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                                                <label htmlFor="password" className="block mt-4 mb-2 text-white text-sm font-medium text-gray-900 dark:text-white">
                                                     Password
                                                 </label>
                                                 <input
@@ -153,8 +154,8 @@ const SignUp = () => {
                                                 />
                                             </div>
 
-                                            <div className="flex items-start">
-                                                <div className="flex items-center h-5">
+                                            <div className="flex items-start ">
+                                                <div className="flex items-center h-5 my-3">
                                                     <input
                                                         id="terms"
                                                         aria-describedby="terms"
@@ -163,7 +164,7 @@ const SignUp = () => {
                                                         required
                                                     />
                                                 </div>
-                                                <div className="ml-3 text-sm">
+                                                <div className="ml-3 my-3 text-sm">
                                                     <label htmlFor="terms" className="font-light text-gray-500 dark:text-gray-300">
                                                         I accept the{' '}
                                                         <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">
@@ -174,25 +175,36 @@ const SignUp = () => {
                                             </div>
                                             <button
                                                 type="submit"
-                                                className="w-full text-black bg-yellow-300 hover:bg-yellow-400 focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center  dark:focus:ring-primary-800"
+                                                className="w-full text-slate-700 bg-yellow-300 hover:bg-yellow-400 focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center  dark:focus:ring-primary-800"
                                             >
                                                 Create an account
                                             </button>
-                                            <button
-                                                onClick={handleGoogle}
-                                                className="w-full text-black  bg-white focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-sm py-2.5 text-center  flex gap-2 justify-center items-center"
-                                            >
-                                                SignUp With <FcGoogle size={20} />
-                                            </button>
 
-                                            <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                                            <div class="inline-flex items-center justify-center w-full ">
+                                                <hr class="w-full h-0.5 my-4 bg-gray-200 border-0 rounded dark:bg-gray-700"/>
+                                                <div class="px-2 text-xs  text-white ">
+                                                        OR
+                                                    </div>
+                                                <hr class="w-full h-0.5 my-4 bg-gray-200 border-0 rounded dark:bg-gray-700" />
+                                            </div>
+
+                                           
+                                        </form>
+                                        <button onClick={handleGoogle} className=" w-full text-sm  px-5 py-2.5 bg-white border flex justify-center gap-2 border-slate-200 dark:border-slate-700 rounded-lg text-slate-700 dark:text-slate-200 hover:border-slate-400 dark:hover:border-slate-500 hover:text-slate-900 dark:hover:text-slate-300 hover:shadow transition duration-150">
+                                            <FcGoogle size={20} />
+                                            <span>Continue with Google</span>
+                                        </button>
+
+
+
+                                        <div >
+                                            <p className="text-center mt-3 text-sm font-light text-gray-500 dark:text-gray-400">
                                                 Already have an account?{' '}
                                                 <Link href="/login" className="font-medium text-yellow-300 hover:underline ">
                                                     Login
                                                 </Link>
-
                                             </p>
-                                        </form>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
