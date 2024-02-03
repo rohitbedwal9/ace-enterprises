0d42ba68ba53cac9a128accf8c2e910a660c216f
Rohit Singh Bedwal <rohitbedwal9@gmail.com>
2024-02-03 16:22:11 +0530

    New Design for project Component

--- src/components/projects/index.js
+++ src/components/projects/index.js
@@ -1,55 +1,56 @@
-import React from "react";
-import Link from "next/link";
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
 
 const data = [
   {
     id: 1,
-    image:
-      "https://t3.ftcdn.net/jpg/03/20/36/32/360_F_320363200_5OmTMzWXKD86jBvsiraMbZCA0uSKyGtt.jpg",
-    title: "Project 1",
-    desc: "Bridge",
+    imgUrl:
+      'https://t3.ftcdn.net/jpg/03/20/36/32/360_F_320363200_5OmTMzWXKD86jBvsiraMbZCA0uSKyGtt.jpg',
+    title: 'Project 1',
+    desc: 'Bridge',
   },
   {
     id: 2,
-    image:
-      "https://media.istockphoto.com/id/526298880/photo/road-rollers-working-on-the-construction-site.jpg?s=612x612&w=0&k=20&c=C7CM-81xFktkVKcJMmC-CgYuE5D2zomncwTv7kZ5h4o=",
-    title: "Project 2",
-    desc: "Road",
+    imgUrl:
+      'https://t3.ftcdn.net/jpg/03/31/59/82/360_F_331598222_tadTdFeI0WxEmyGFoWuMTlEK1jNu2Xhl.jpg',
+    title: 'Project 2',
+    desc: 'Road',
   },
   {
     id: 3,
-    image:
-      "https://t3.ftcdn.net/jpg/03/31/59/82/360_F_331598222_tadTdFeI0WxEmyGFoWuMTlEK1jNu2Xhl.jpg",
-    title: "Project 3",
-    desc: "Buiding",
+    imgUrl:
+      'https://t3.ftcdn.net/jpg/03/31/59/82/360_F_331598222_tadTdFeI0WxEmyGFoWuMTlEK1jNu2Xhl.jpg',
+    title: 'Project 3',
+    desc: 'Buiding',
   },
   {
     id: 4,
-    image:
-      "https://t3.ftcdn.net/jpg/03/31/59/82/360_F_331598222_tadTdFeI0WxEmyGFoWuMTlEK1jNu2Xhl.jpg",
-    title: "Project 4",
-    desc: "Buiding",
+    imgUrl:
+      'https://t3.ftcdn.net/jpg/03/31/59/82/360_F_331598222_tadTdFeI0WxEmyGFoWuMTlEK1jNu2Xhl.jpg',
+    title: 'Project 4',
+    desc: 'Buiding',
   },
   {
     id: 5,
-    image:
-      "https://t3.ftcdn.net/jpg/03/31/59/82/360_F_331598222_tadTdFeI0WxEmyGFoWuMTlEK1jNu2Xhl.jpg",
-    title: "Project 5",
-    desc: "Buiding",
+    imgUrl:
+      'https://t3.ftcdn.net/jpg/03/31/59/82/360_F_331598222_tadTdFeI0WxEmyGFoWuMTlEK1jNu2Xhl.jpg',
+    title: 'Project 5',
+    desc: 'Buiding',
   },
   {
     id: 6,
-    image:
-      "https://t3.ftcdn.net/jpg/03/31/59/82/360_F_331598222_tadTdFeI0WxEmyGFoWuMTlEK1jNu2Xhl.jpg",
-    title: "Project 6",
-    desc: "Buiding",
+    imgUrl:
+      'https://t3.ftcdn.net/jpg/03/31/59/82/360_F_331598222_tadTdFeI0WxEmyGFoWuMTlEK1jNu2Xhl.jpg',
+    title: 'Project 6',
+    desc: 'Buiding',
   },
   {
     id: 7,
-    image:
-      "https://t3.ftcdn.net/jpg/03/31/59/82/360_F_331598222_tadTdFeI0WxEmyGFoWuMTlEK1jNu2Xhl.jpg",
-    title: "Project 7",
-    desc: "Buiding",
+    imgUrl:
+      'https://t3.ftcdn.net/jpg/03/31/59/82/360_F_331598222_tadTdFeI0WxEmyGFoWuMTlEK1jNu2Xhl.jpg',
+    title: 'Project 7',
+    desc: 'Buiding',
   },
 ];
 
@@ -64,7 +65,12 @@ export const Projects = () => {
           <h1 className="text-2xl md:text-4xl font-bold w-max text-black px-2 bg-yellow-300">
             Recent Projects
           </h1>
-          <p className="text-xl font-semibold">We design curated residential spaces that are a beautiful reflection of what’s most important to you. Our portfolio spans all style disciplines, from traditional to contemporary and everything in between.</p>
+          <p className="text-xl font-semibold">
+            We design curated residential spaces that are a beautiful reflection
+            of what’s most important to you. Our portfolio spans all style
+            disciplines, from traditional to contemporary and everything in
+            between.
+          </p>
         </div>
         <div className="mx-auto justify-self-center text-center md:w-[20%] ">
           <Link
@@ -76,12 +82,14 @@ export const Projects = () => {
         </div>
       </div>
       <div className="w-full flex-wrap h-100 flex md:flex-row flex-col  justify-center items-center">
-        {data.map((project, index) => (
+        {data.map((project) => (
           <div key={project.id} className=" m-5 rounded-lg shadow-lg bg-white">
-            <img
+            <Image
               className="object-cover w-90 h-72 sm:w-64 rounded-lg"
-              src={project.image}
+              src={project.imgUrl}
               alt={project.desc}
+              width={300}
+              height={300}
             />
 
             <div className="p-4 sm:px-6 sm:py-4 text-center">
