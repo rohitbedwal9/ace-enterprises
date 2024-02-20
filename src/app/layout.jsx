65f12405eb2acbe17993fbe7e62f9116b5e63a2b
Rohit Singh Bedwal <rohitbedwal9@gmail.com>
2024-02-20 21:37:00 +0530

    added meta description

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -7,6 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Ace Enterprises",
+  description: "Ace Enterprises is a distinguished civil construction company that has been setting the standard since 2012. Renowned for our unwavering commitment to innovation and quality, we have left an indelible mark on numerous government projects, proudly collaborating with both the Central and Uttarakhand Government.",
 };
 
 export default function RootLayout({ children }) {
