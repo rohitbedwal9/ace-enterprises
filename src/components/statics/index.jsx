ff8c99eac9d98b527b12e16ec693d3295a0b71ac
Rohit Singh Bedwal <rohitbedwal9@gmail.com>
2024-02-21 17:24:57 +0530

    removed eslint

--- src/components/statics/index.jsx
+++ src/components/statics/index.jsx
@@ -4,7 +4,7 @@ export const Statics = () => {
   return (
     <div className="flex  shadow-xl flex-col w-full gap-10 md:flex-row justify-around md:px-56 py-10 items-center h-full md:h-56 bg-white">
       <div className="flex flex-col gap-2 justify-center  items-center">
-        <span className="flex items-center text-3xl sm:text-4xl md:text-5xl font-bold">
+        <span className="flex items-center gap-2 text-3xl sm:text-4xl md:text-5xl font-bold">
           <AnimatedNumbers
             includeComma
             transitions={(index) => ({
@@ -21,7 +21,7 @@ export const Statics = () => {
         <p className="text-xl sm:text-2xl text-center ">Sucessfull Projects</p>
       </div>
       <div className="flex flex-col gap-2 justify-center items-center">
-        <span className=" flex items-center text-3xl sm:text-4xl md:text-5xl font-bold">
+        <span className=" flex items-center gap-2 text-3xl sm:text-4xl md:text-5xl font-bold">
           <AnimatedNumbers
             includeComma
             transitions={(index) => ({
@@ -38,7 +38,7 @@ export const Statics = () => {
         <p className="text-xl sm:text-2xl text-center">Team Members</p>
       </div>
       <div className="flex flex-col gap-2 justify-center items-center">
-        <span className=" flex items-center text-3xl sm:text-4xl md:text-5xl font-bold">
+        <span className=" flex items-center gap-2 text-3xl sm:text-4xl md:text-5xl font-bold">
           <AnimatedNumbers
             includeComma
             transitions={(index) => ({
@@ -55,7 +55,7 @@ export const Statics = () => {
         <p className="text-xl sm:text-2xl text-center">Happy Customers</p>
       </div>
       <div className="flex flex-col gap-2 justify-center items-center">
-        <span className="flex items-center text-3xl sm:text-4xl md:text-5xl font-bold">
+        <span className="flex items-center text-3xl gap-2 sm:text-4xl md:text-5xl font-bold">
           <AnimatedNumbers
             includeComma
             transitions={(index) => ({
