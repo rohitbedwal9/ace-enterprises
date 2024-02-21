5836ed0f6f5a8b07215db872a8cc578bcccdabe7
Rohit Singh Bedwal <rohitbedwal9@gmail.com>
2024-02-21 16:57:45 +0530

    added animation in stats

--- src/components/statics/index.jsx
+++ src/components/statics/index.jsx
@@ -1,20 +1,74 @@
+'use strict';
+import AnimatedNumbers from 'react-animated-numbers';
 export const Statics = () => {
   return (
     <div className="flex  shadow-xl flex-col w-full gap-10 md:flex-row justify-around md:px-56 py-10 items-center h-full md:h-56 bg-white">
       <div className="flex flex-col gap-2 justify-center  items-center">
-        <p className="text-3xl sm:text-4xl md:text-5xl font-bold">350+</p>
+        <span className="flex items-center text-3xl sm:text-4xl md:text-5xl font-bold">
+          <AnimatedNumbers
+            includeComma
+            transitions={(index) => ({
+              type: 'up',
+              duration: index + 0.3,
+            })}
+            animateToNumber={25}
+            fontStyle={{
+              fontSize: 40,
+            }}
+          />
+          <span className="text-xl sm:text-2xl md:text-3xl ">+</span>
+        </span>
         <p className="text-xl sm:text-2xl text-center ">Sucessfull Projects</p>
       </div>
       <div className="flex flex-col gap-2 justify-center items-center">
-        <p className="text-3xl sm:text-4xl md:text-5xl font-bold">24+</p>
+        <span className=" flex items-center text-3xl sm:text-4xl md:text-5xl font-bold">
+          <AnimatedNumbers
+            includeComma
+            transitions={(index) => ({
+              type: 'up',
+              duration: index + 0.3,
+            })}
+            animateToNumber={24}
+            fontStyle={{
+              fontSize: 40,
+            }}
+          />
+          <span className="text-xl sm:text-2xl md:text-3xl ">+</span>
+        </span>
         <p className="text-xl sm:text-2xl text-center">Team Members</p>
       </div>
       <div className="flex flex-col gap-2 justify-center items-center">
-        <p className="text-3xl sm:text-4xl md:text-5xl font-bold">50+</p>
+        <span className=" flex items-center text-3xl sm:text-4xl md:text-5xl font-bold">
+          <AnimatedNumbers
+            includeComma
+            transitions={(index) => ({
+              type: 'up',
+              duration: index + 0.3,
+            })}
+            animateToNumber={50}
+            fontStyle={{
+              fontSize: 40,
+            }}
+          />
+          <span className="text-xl sm:text-2xl md:text-3xl ">+</span>
+        </span>
         <p className="text-xl sm:text-2xl text-center">Happy Customers</p>
       </div>
       <div className="flex flex-col gap-2 justify-center items-center">
-        <p className="text-3xl sm:text-4xl md:text-5xl font-bold">100%</p>
+        <span className="flex items-center text-3xl sm:text-4xl md:text-5xl font-bold">
+          <AnimatedNumbers
+            includeComma
+            transitions={(index) => ({
+              type: 'up',
+              duration: index + 0.3,
+            })}
+            animateToNumber={100}
+            fontStyle={{
+              fontSize: 40,
+            }}
+          />
+          <span className="text-xl sm:text-2xl md:text-3xl ">%</span>
+        </span>
         <p className="text-xl sm:text-2xl text-center">Client Satisfaction</p>
       </div>
     </div>
